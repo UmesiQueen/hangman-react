6e76a5987ff34f5915fd15c4e312b106a51fbb3a
Queen <uchezumez@gmail.com>
2023-04-15 05:13:19 +0100

    Enable hint feature

--- src/components/Aside/Hint.tsx
+++ src/components/Aside/Hint.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../../App";
 
 const Hint: React.FC = () => {
-  return <div>Hint</div>;
+  const { word } = useContext(AppContext);
+
+  return (
+    <div className=" w-1/2 md:w-1/4 h-fit p-5 bg-white text-blackF absolute top-[18%] md:top-[24%] left-[40%] md:left-1/2 shadow-xl border">
+      <p className="italic">{word[0].partOfSpeech}</p>
+      <dl>
+        <dt>
+          <h2>Definition</h2>
+        </dt>
+        <dd>{word[0].definition}</dd>
+      </dl>
+    </div>
+  );
 };
 
 export default Hint;
--- src/components/Head/SubHead.tsx
+++ src/components/Head/SubHead.tsx
@@ -1,8 +1,19 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { AppContext } from "../../App";
+import Hint from "../Aside/Hint";
 
 const SubHead: React.FC = () => {
-  const { level, selected } = useContext(AppContext);
+  const { level, selected, setSelected } = useContext(AppContext);
+  const [clicked, setClicked] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!selected.mismatch.includes("hint") && clicked)
+      setSelected((prev: any) => {
+        return { ...prev, mismatch: [...prev.mismatch, "hint"] };
+      });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [clicked]);
 
   return (
     <div className=" w-full flex justify-between px-1">
@@ -10,7 +21,20 @@ const SubHead: React.FC = () => {
         {" X ".repeat(6 - Number(selected.mismatch.length))}
       </p>
       <h2>Level {level}</h2>
-      <button>Hint</button>
+      <button
+        className="hint px-2 bg-black text-white relative inline-block border border-solid border-black hover:scale-105"
+        onClick={() => setClicked(true)}
+        onBlur={() => setClicked(false)}
+      >
+        Hint
+        <span
+          className="tooltip w-40 p-1 mt-1 bg-[#686868] text-white text-left rounded-md
+        absolute z-1 top-full right-1/2 invisible"
+        >
+          Reveal Hint. <br /> PS, you will loose a life
+        </span>
+      </button>
+      {clicked && <Hint />}
     </div>
   );
 };
